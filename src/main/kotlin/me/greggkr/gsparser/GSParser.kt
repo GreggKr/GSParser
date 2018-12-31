13f65295709289593b52685eac13a8a9b3b3fbcb
Grzegorz <greggkrzykowski@gmail.com>
2018-12-30 19:05:25 -0600

    other stuff

--- src/main/kotlin/me/greggkr/gsparser/GSParser.kt
+++ src/main/kotlin/me/greggkr/gsparser/GSParser.kt
@@ -13,10 +13,32 @@ object GSParser {
             .replace(Regex("<script.*?>.*?</script>", RegexOption.DOT_MATCHES_ALL), "")
             // Remove noscript Tag
             .replace(Regex("<noscript>.*?</noscript>", RegexOption.DOT_MATCHES_ALL), "")
-            // Replaces notes
-            .replace(Regex("<td class=\"AssignmentNote\"(.*?)>(.*?)</td>", RegexOption.DOT_MATCHES_ALL), "Note:'\$1'")
             // Remove the weird input things
             .replace(Regex("<input.*?( /)?>(.*?</input>)?", RegexOption.DOT_MATCHES_ALL), "")
+            // Replace Rows
+            .replace(Regex("<tr.*?>(.*?)</tr>", RegexOption.DOT_MATCHES_ALL), "tr: \"\"\$1\"\"\n\n")
+            // Replaces notes
+            .replace(Regex("<td class=\"AssignmentNote\"(.*?)>(.*?)</td>", RegexOption.DOT_MATCHES_ALL), "Note:'\$2'")
+            // Replace Names
+            .replace(Regex("<td class=\"AssignmentName\"(.*?)>(.*?)</td>", RegexOption.DOT_MATCHES_ALL), "Name:'\$2'")
+            // Replace Date Assigned
+            .replace(
+                Regex("<td class=\"DateAssigned\"(.*?)>(.*?)</td>", RegexOption.DOT_MATCHES_ALL),
+                "Assigned:'\$2'"
+            )
+            // Replace Date Due
+            .replace(
+                Regex("<td class=\"DateDue\"(.*?)>(.*?)</td>", RegexOption.DOT_MATCHES_ALL),
+                "Due:'\$2'"
+            )
+            // Replace Grade:
+            .replace(
+                Regex("<td class=\"AssignmentGrade\"(.*?)>(.*?)</td>", RegexOption.DOT_MATCHES_ALL),
+                "Grade:'\$2'"
+            )
+            // Very annoying things at the end of each tr
+            .replace("<td></td>", "")
+        // Renive
     }
 }
 
